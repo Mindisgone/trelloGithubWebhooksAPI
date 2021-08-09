4536a826fdbaecea4c9981ebde107c13a001008d
Tom Collins <teecee@teeceetech.com>
2021-08-09 17:03:56 -0400

    feat: initial commit

---
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/TrelloGithubWebhooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.teeceetech.trellogithubwebhooksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloGithubWebhooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloGithubWebhooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teeceetech/trellogithubwebhooksapi/TrelloGithubWebhooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teeceetech.trellogithubwebhooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloGithubWebhooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
