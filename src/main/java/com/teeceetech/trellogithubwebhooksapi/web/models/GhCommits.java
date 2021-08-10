aae53586ec8fdf35a37597fdb620d39b0343c200
Tom Collins <10360366+Mindisgone@users.noreply.github.com>
2021-08-10 01:38:47 -0400

    feat: added logic for building PR merged messages (#2)
    * feat: added logic for building PR merged messages
    
    * changed prop PR id to number
    
    * attempting to fix yes pipe in travis command
    
    Co-authored-by: Tom Collins <teecee@teeceetech.com>

---
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/web/models/GhCommits.java
@@ -0,0 +1,12 @@
+package com.teeceetech.trellogithubwebhooksapi.web.models;
+
+public class GhCommits {
+    public String href;
+
+    public GhCommits() {
+    }
+
+    public String getHref() {
+        return href;
+    }
+}
---
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/web/models/GhSelf.java
@@ -0,0 +1,12 @@
+package com.teeceetech.trellogithubwebhooksapi.web.models;
+
+public class GhSelf {
+    public String href;
+
+    public GhSelf() {
+    }
+
+    public String getHref() {
+        return href;
+    }
+}
