fbfa55a3db3f2f556a8d678070f2ede1c5c48a68
Tom Collins <10360366+Mindisgone@users.noreply.github.com>
2021-08-10 21:49:14 -0400

    WIP

--- src/test/java/com/teeceetech/trellogithubwebhooksapi/TrelloGithubWebhooksApiApplicationTests.java
+++ src/test/java/com/teeceetech/trellogithubwebhooksapi/TrelloGithubWebhooksApiApplicationTests.java
@@ -1,9 +1,12 @@
 package com.teeceetech.trellogithubwebhooksapi;
 
+import com.teeceetech.trellogithubwebhooksapi.config.TrelloConfigProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableConfigurationProperties(TrelloConfigProperties.class)
 class TrelloGithubWebhooksApiApplicationTests {
 
 	@Test
