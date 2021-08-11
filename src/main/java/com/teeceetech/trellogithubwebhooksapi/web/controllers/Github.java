2866259a34d15a85e16e68dd54fbefaf995f1b5e
Tom Collins <10360366+Mindisgone@users.noreply.github.com>
2021-08-11 01:03:44 -0400

    fix: changed PR url to correct property

--- src/main/java/com/teeceetech/trellogithubwebhooksapi/web/controllers/Github.java
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/web/controllers/Github.java
@@ -69,14 +69,14 @@ public class Github {
     }
 
     private void buildPrOpenComment(GhRoot ghRoot, String trelloKey, String token) {
-        String comment = "Opened PR " + ghRoot.pull_request.number + ", link -> [PR](" + ghRoot.pull_request.url + ")";
+        String comment = "Opened PR " + ghRoot.pull_request.number + ", link -> [PR](" + ghRoot.pull_request.html_url + ")";
 
         addCardComment(getCardId(ghRoot.pull_request.head.ref, trelloKey, token), comment, trelloKey, token);
     }
 
     private void buildPrMergedComment(GhRoot ghRoot, String trelloKey, String token) {
         String comment = "Merged PR " + ghRoot.pull_request.number + " from " +
-                ghRoot.pull_request.merged_by.login + " " + ghRoot.pull_request.url;
+                ghRoot.pull_request.merged_by.login + " " + ghRoot.pull_request.html_url;
 
         addCardComment(getCardId(ghRoot.pull_request.head.ref, trelloKey, token), comment, trelloKey, token);
     }
