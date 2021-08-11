ddbc62be0edc7001c004895f9076b2c68040fb58
Tom Collins <10360366+Mindisgone@users.noreply.github.com>
2021-08-10 20:04:22 -0400

    trello-card-search (#8)
    * WIP
    
    * feat: trello card search and add card comment functionality is working

--- src/main/java/com/teeceetech/trellogithubwebhooksapi/web/controllers/Github.java
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/web/controllers/Github.java
@@ -1,8 +1,8 @@
 package com.teeceetech.trellogithubwebhooksapi.web.controllers;
 
 import com.teeceetech.trellogithubwebhooksapi.web.models.github.GhRoot;
-import com.teeceetech.trellogithubwebhooksapi.web.models.trello.Card;
 import com.teeceetech.trellogithubwebhooksapi.web.models.trello.Root;
+import com.teeceetech.trellogithubwebhooksapi.web.models.trello.Term;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Objects;
-
 @RestController
 public class Github {
     @Value("${trello.key}")
@@ -25,7 +22,6 @@ public class Github {
             private String trelloToken;
 
     Logger logger = LoggerFactory.getLogger(Github.class);
-
     RestTemplate restTemplate = new RestTemplate();
 
     @Autowired
@@ -34,25 +30,16 @@ public class Github {
     @RequestMapping(value = "/api/github", method = RequestMethod.POST)
     public void receiveGithubMessage(@RequestBody GhRoot message){
         if (message.action != null && message.getAction().equals("opened")){
-            logger.info("PR OPENED, url = " + message.pull_request.url);
-            logger.info("PR OPENED, id = " + message.pull_request.number);
-            logger.info("PR OPENED, author is = " + message.pull_request.assignee.login);
-            logger.info("PR OPENED, branch is = " + message.pull_request.head.ref);
-
-            logger.info("trello key = " + trelloKey);
-            logger.info("trello token = " + trelloToken);
-            logger.info("Card ID is = " + getCardId(message.pull_request.head.ref));
+            buildPrOpenComment(message);
         }
 
         if (message.action != null && message.getAction().equals("closed") && message.pull_request.merged){
-            logger.info("PR CLOSED, url = " + message.pull_request.url);
-            logger.info("PR CLOSED, id = " + message.pull_request.number);
-            logger.info("PR CLOSED, author is = " + message.pull_request.merged_by.login);
-            logger.info("PR CLOSED, branch is = " + message.pull_request.head.ref);
+            buildPrMergedComment(message);
+        }
 
-            logger.info("trello key = " + trelloKey);
-            logger.info("trello token = " + trelloToken);
-            logger.info("Card ID is = " + getCardId(message.pull_request.head.ref));
+        if (message.action != null && message.getAction().equals("created")){
+            // PR title needs to be the same as branch name and trello card
+            buildPrComment(message);
         }
     }
 
@@ -60,13 +47,45 @@ public class Github {
         String cardId = "";
         String url = "https://api.trello.com/1/search?modelTypes=cards&query=" +
                 name + "&key=" + trelloKey + "&token=" + trelloToken;
-        // require non null
-        List<Card> cards = Objects.requireNonNull(restTemplate.getForObject(url, Root.class)).cards;
 
-        if (cards.size() == 1){
-            cardId = cards.get(0).id;
+        Root root = restTemplate.getForObject(url, Root.class);
+
+        if (root != null && root.cards.size() == 1){
+            cardId = root.cards.get(0).id;
+        } else {
+            logger.error("search produced no results");
         }
 
         return cardId;
+    }
+
+    private void addCardComment(String ID, String text) {
+        String url = "https://api.trello.com/1/cards/" +
+                ID + "/actions/comments?key=" + trelloKey + "&token=" + trelloToken;
+        Term term = new Term();
+        term.setText(text);
+
+        logger.info("GITHUB CONTROLLER: adding comment " + text + " to card " + ID);
+        restTemplate.postForLocation(url, term);
+    }
+
+    private void buildPrOpenComment(GhRoot ghRoot) {
+        String comment = "Opened PR " + ghRoot.pull_request.number + " " + ghRoot.pull_request.url;
+
+        addCardComment(getCardId(ghRoot.pull_request.head.ref), comment);
+    }
+
+    private void buildPrMergedComment(GhRoot ghRoot) {
+        String comment = "Merged PR " + ghRoot.pull_request.number + " from " +
+                ghRoot.pull_request.merged_by.login + " " + ghRoot.pull_request.url;
+
+        addCardComment(getCardId(ghRoot.pull_request.head.ref), comment);
+    }
+
+    private void buildPrComment(GhRoot ghRoot) {
+        String comment = "{Github User " + ghRoot.comment.user.login + " commented " +
+                ghRoot.comment.body + " on PR " + ghRoot.issue.number + " " + ghRoot.comment.html_url + "}";
+
+        addCardComment(getCardId(ghRoot.issue.title), comment);
     }
 }
