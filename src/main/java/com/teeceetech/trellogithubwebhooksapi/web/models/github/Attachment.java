5ad8531f3d6224b20d95ca88b16043cbdbe91732
Tom Collins <10360366+Mindisgone@users.noreply.github.com>
2021-08-12 00:33:58 -0400

    feat: using trello api instead of butler to attach pr link

---
+++ src/main/java/com/teeceetech/trellogithubwebhooksapi/web/models/github/Attachment.java
@@ -0,0 +1,16 @@
+package com.teeceetech.trellogithubwebhooksapi.web.models.github;
+
+public class Attachment {
+    String url;
+
+    public Attachment() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
